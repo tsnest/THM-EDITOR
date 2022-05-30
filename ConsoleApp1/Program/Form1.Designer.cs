3f4419b120f839cd3ef1e6218ac42e7ebec65079
i-love-kfc <stepadunaevskij@gmail.com>
2022-05-30 20:43:57 +0400

    Update Form1.Designer.cs

--- ConsoleApp1/Program/Form1.Designer.cs
+++ ConsoleApp1/Program/Form1.Designer.cs
@@ -63,6 +63,7 @@ namespace ConsoleApp1
             this.label14 = new System.Windows.Forms.Label();
             this.label15 = new System.Windows.Forms.Label();
             this.comboBox4 = new System.Windows.Forms.ComboBox();
+            this.label16 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -107,7 +108,7 @@ namespace ConsoleApp1
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Location = new System.Drawing.Point(12, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(66, 13);
             this.label1.TabIndex = 3;
@@ -116,7 +117,7 @@ namespace ConsoleApp1
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 53);
+            this.label2.Location = new System.Drawing.Point(12, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(75, 13);
             this.label2.TabIndex = 4;
@@ -148,7 +149,7 @@ namespace ConsoleApp1
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 80);
+            this.label3.Location = new System.Drawing.Point(12, 77);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(63, 13);
             this.label3.TabIndex = 6;
@@ -178,7 +179,7 @@ namespace ConsoleApp1
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 277);
+            this.label5.Location = new System.Drawing.Point(6, 278);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(63, 13);
             this.label5.TabIndex = 9;
@@ -187,7 +188,7 @@ namespace ConsoleApp1
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 302);
+            this.label6.Location = new System.Drawing.Point(6, 305);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(92, 13);
             this.label6.TabIndex = 10;
@@ -217,7 +218,7 @@ namespace ConsoleApp1
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(45, 141);
+            this.label7.Location = new System.Drawing.Point(49, 141);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(64, 13);
             this.label7.TabIndex = 14;
@@ -226,7 +227,7 @@ namespace ConsoleApp1
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(151, 141);
+            this.label8.Location = new System.Drawing.Point(148, 141);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(57, 13);
             this.label8.TabIndex = 15;
@@ -385,11 +386,21 @@ namespace ConsoleApp1
             this.comboBox4.Size = new System.Drawing.Size(195, 21);
             this.comboBox4.TabIndex = 33;
             // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Location = new System.Drawing.Point(158, 328);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(153, 13);
+            this.label16.TabIndex = 34;
+            this.label16.Text = "Created by i-love-kfc (SOC:GA)";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(448, 329);
+            this.ClientSize = new System.Drawing.Size(448, 350);
+            this.Controls.Add(this.label16);
             this.Controls.Add(this.comboBox4);
             this.Controls.Add(this.label15);
             this.Controls.Add(this.label14);
@@ -468,5 +479,6 @@ namespace ConsoleApp1
         private System.Windows.Forms.Label label14;
         private System.Windows.Forms.Label label15;
         private System.Windows.Forms.ComboBox comboBox4;
+        private System.Windows.Forms.Label label16;
     }
 }
